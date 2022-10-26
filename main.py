9bb98764eb04fca01f57a5feffa80425ac82db8c
MSI <jaminurmatov04@gmail.com>
2022-10-26 11:53:50 +0600

    asdadsadasdasdsadsa

---
+++ main.py
@@ -0,0 +1,16 @@
+class SuperJundev():
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def __str__(self):
+        return f'твое имя - {self.name}\n' \
+               f'твое значение - {self.value}'
+JunDev1 = SuperJundev(name='Вася', value=True)
+print(JunDev1)
+obje 
+asdasdsadsad
+
+
+
+
